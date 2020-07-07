d35ceccc98bd4fc400427c84192c3230fa274e4c
xuyou1999 <yx1711@nyu.edu>
2020-07-07 13:34:26 +0800

    xuyou commit

--- you_xu.py
+++ you_xu.py
@@ -9,6 +9,12 @@ def clean_entry_date(sheet):
     lst = []
     for row in range(1, sheet.max_row):
         lst.append(sheet.cell(row, openpyxl.utils.column_index_from_string('AO')).value)
+    for i in range(len(lst)):
+        value = lst[i]
+        if type(value) == type('1'):
+            if len(value) == 8:
+                year = value[-4:]
+                
     return lst
 
 print(clean_entry_date(sheet))
