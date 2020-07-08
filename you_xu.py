6991667a56821cac81cdeb261bdd6e5a15bd7a73
xuyou1999 <yx1711@nyu.edu>
2020-07-08 23:12:32 +0800

    xuyou commit

--- you_xu.py
+++ you_xu.py
@@ -11,13 +11,17 @@ def get_col_number(sheet, name):
     for col in range(1, sheet.max_column + 1):
         if sheet.cell(1, col).value == name:
             return col
+
+def get_data_list(sheet, col):
+    lst = []
+    for row in range(2, sheet.max_row + 1):
+        lst.append(sheet.cell(row, col).value)
+    return lst
     
 #get all entry date infor into a list
 def clean_entry_date(sheet):
-    lst = []
     col = get_col_number(sheet, 'ENTRYDATE')
-    for row in range(2, sheet.max_row + 1):
-        lst.append(sheet.cell(row, col).value)
+    lst = get_data_list(sheet, col)
 
     years = []
     months = []
@@ -43,13 +47,68 @@ def clean_entry_date(sheet):
         years.append(year)
         months.append(month)
         days.append(day)
-    
-    #new_column = pd.DataFrame({'new_header': ['new_value_1', 'new_value_2', 'new_value_3']})
+
     df = pd.read_csv("you_xu.csv")
     df["ENTRYDATE_YEAR"] = years
     df['ENTRYDATE_MONTH'] = months
     df['ENTRYDATE_DAY'] = days
     df.to_csv("you_xu.csv", index=False)
     
+def clean_entry_date2(sheet):
+    col = get_col_number(sheet, 'ENTRYDATE2')
+    lst = get_data_list(sheet, col)
 
-clean_entry_date(sheet)
+    years = []
+    months = []
+    days = []
+    for i in range(len(lst)):
+        value = lst[i]
+        if type(value) == type('1'):
+            if len(value) == 8:
+                year = value[-4:]
+                month = value[0:2]
+                if value[2:4] == '  ':
+                    day = None
+                else:
+                    day = value[2:4]
+            elif len(value) == 4:
+                year = value
+                month = None
+                day = None
+        else:
+            year = None
+            month = None
+            day = None
+        years.append(year)
+        months.append(month)
+        days.append(day)
+
+    df = pd.read_csv("you_xu.csv")
+    df["ENTRYDATE2_YEAR"] = years
+    df['ENTRYDATE2_MONTH'] = months
+    df['ENTRYDATE2_DAY'] = days
+    df.to_csv("you_xu.csv", index=False)
+
+def clean_docdate(sheet):
+    col = get_col_number(sheet, 'DOCDATE')
+    lst = get_data_list(sheet, col)
+
+    first = []
+    last = []
+    for date in lst:
+        if type(date) == type('1') and len(date) == 8:
+            first.append(date[:4])
+            last.append(date[-4:])
+        else:
+            first.append(None)
+            last.append(None)
+
+    df = pd.read_csv("you_xu.csv")
+    df["DOCDATE1"] = first
+    df['DOCDATE2'] = last
+    df.to_csv("you_xu.csv", index=False)
+
+if __name__ == '__main__':
+    clean_entry_date(sheet)
+    clean_entry_date2(sheet)
+    clean_docdate(sheet)
